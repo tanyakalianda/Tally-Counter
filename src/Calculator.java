b86b2b285695ad59281ed20aff3bc009f58fe751
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-09-26 00:04:49 -0400

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Tanya Kalianda
+ * Simple Calculator
+ */
+public class Calculator 
+{
+	public int add(int num1, int num2)
+	{
+			int ans = num1 + num2;
+			return ans; 
+	}
+	
+	public int subtract(int num1, int num2)
+	{
+			int ans = num1 - num2;
+			return ans; 
+
+	}
+}

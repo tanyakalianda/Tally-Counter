d47d58221e207325fca5a3a1585ba4a4fec32777
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-09-26 00:05:06 -0400

    Add files via upload

---
+++ src/TallyCounterMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Tanya Kalianda	
+ * Tally Counter Main
+ */
+public class TallyCounterMain 
+{
+	Private int count; 
+	
+	Public TallyCounterMain()
+	{
+		count = 0;
+	}
+	
+	Public int getCount()
+	
+	
+	
+	Counter tally = new Counter; 
+		return: tally.getCount();
+}
